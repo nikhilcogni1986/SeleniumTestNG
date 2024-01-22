95c4255ba6bf2480c72dee92c6647028e0d9c956
Nikhil <nikhilcogni@gmail.com>
2024-01-22 19:50:36 +0000

    Added case for Edge browser

--- src/test/java/com/techstack/TestComponents/BaseTest.java
+++ src/test/java/com/techstack/TestComponents/BaseTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -41,6 +42,8 @@ public class BaseTest
             case "Firefox":
                 driver = new FirefoxDriver();
                 break;
+            case "Edge":
+                driver = new EdgeDriver();
             default:
                 System.out.println("No Browser name provided");
                 break;
